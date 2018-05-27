ba92b2c1bb283bdf2cf730000921d86ba0750009
Rafael Marinho de Araújo <rma6@cin.ufpe.br>
2018-05-27 17:44:29 -0300

    Update main1.c

--- main1.c
+++ main1.c
@@ -8,25 +8,24 @@ int main()
 	char* inst = (char*)malloc(sizeof(char)*10);
 	char* buf = (char*)malloc(sizeof(char)*10);
 	int result;
-	char eflag;
 
 	fgets(inst, 10, stdin);
 	fputs(inst, file);
-    fclose(file);
-    while(1)
-    {
-    	file = fopen("/mnt/ramdisk/21", "r+");
-    	fgets(buf, 10, file);
-    	if(buf[0]=='$')
-    	{
-    		buf++;
-    		file = fopen("/mnt/ramdisk/21", "w");
-    		fclose(file);
+	fclose(file);
+	while(1)
+	{
+		file = fopen("/mnt/ramdisk/21", "r+");
+		fgets(buf, 10, file);
+		if(buf[0]=='$')
+		{
+			buf++;
+			file = fopen("/mnt/ramdisk/21", "w");
+			fclose(file);
 
-    		sscanf(buf, "%i", &result);
-		    printf("%i", result);
-		    break;
-    	}
-    	fclose(file);
-    }
+			sscanf(buf, "%i", &result);
+			printf("%i", result);
+			break;
+		}
+		fclose(file);
+	}
 }
