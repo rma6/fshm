3dcf7faec425c8e3766a57bdc09d7880e23a9068
Rafael Marinho de Araújo <rma6@cin.ufpe.br>
2018-05-27 17:40:59 -0300

    Create main1.c

---
+++ main1.c
@@ -0,0 +1,32 @@
+# include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE* file = fopen("/mnt/ramdisk/12", "w");
+
+	char* inst = (char*)malloc(sizeof(char)*10);
+	char* buf = (char*)malloc(sizeof(char)*10);
+	int result;
+	char eflag;
+
+	fgets(inst, 10, stdin);
+	fputs(inst, file);
+    fclose(file);
+    while(1)
+    {
+    	file = fopen("/mnt/ramdisk/21", "r+");
+    	fgets(buf, 10, file);
+    	if(buf[0]=='$')
+    	{
+    		buf++;
+    		file = fopen("/mnt/ramdisk/21", "w");
+    		fclose(file);
+
+    		sscanf(buf, "%i", &result);
+		    printf("%i", result);
+		    break;
+    	}
+    	fclose(file);
+    }
+}
